502bc39726ff94dfc1b4bc086842b10dd00395db
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-11-05 19:45:42 +0545

    Part13 exercise is completed upto part13.14

--- blog/controllers/blogs.js
+++ blog/controllers/blogs.js
@@ -17,19 +17,29 @@ const blogFinder = async (req, res, next) => {
 router.get("/", async (req, res, next) => {
   try {
     const where = {};
-    
+
     if (req.query.search) {
-      where.title = {
-        [Op.iLike]: `%${req.query.search}%`,
-      };
+      where[Op.or] = [
+        {
+          title: {
+            [Op.iLike]: `%${req.query.search}%`,
+          },
+        },
+        {
+          author: {
+            [Op.iLike]: `%${req.query.search}%`,
+          },
+        },
+      ];
     }
+
     const blogs = await Blog.findAll({
       attributes: { exclude: ["userId"] },
       include: {
         model: User,
         attributes: ["name", "username"],
       },
-      where
+      where,
     });
     res.json(blogs);
   } catch (error) {
