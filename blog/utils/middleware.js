332eedbbe8e957f6b4817956232081ce07bf1407
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-11-12 10:41:03 +0545

    added some middleware config files

--- blog/utils/middleware.js
+++ blog/utils/middleware.js
@@ -1,5 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../utils/config");
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
+  next();
 };
 
 const errorHandler = (error, req, res, next) => {
@@ -34,5 +51,6 @@ const errorHandler = (error, req, res, next) => {
 
 module.exports = {
   errorHandler,
+  tokenExtractor,
   unknownEndpoint,
 };
